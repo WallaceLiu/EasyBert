df2e0c9e342b0d33d368c90bede3b3633458abec
liuning <liuning@bjxy.com>
2023-03-20 13:30:36 +0800

    cache

--- utils/persist.py
+++ utils/persist.py
@@ -15,14 +15,14 @@ mycache = RedisPipeline(**{
     'db': config['REDIS_DBINDEX_0'],
 })
 
-field = ['lev1_name', 'lev2_name', 'lev3_name', 'location', 'positive', 'neutral', 'negative', ]
 cache_key_template = 'dazhongdianping_comments_sentiment_{lev1_name}_{lev2_name}_{lev3_name}_{location}'
 
 
-def p(data):
-    # redis
-    cache_key = cache_key_template.format(lev1_name=data[0], lev2_name=data[1], lev3_name=data[2], location=data[3])
-    r = mycache.hmset(cache_key, {
+def cache(dataset_list):
+    """cache positive, neutral, negative percentage
+
+    list, each of item
+     {
         'lev1_name': data[0],
         'lev2_name': data[1],
         'lev3_name': data[2],
@@ -30,5 +30,14 @@ def p(data):
         'positive': data[4],
         'neutral': data[5],
         'negative': data[6],
-    })
-    # r=mycache.p
+    }
+
+    """
+
+    dataset = [(cache_key_template.format(lev1_name=data['lev1_name'],
+                                          lev2_name=data['lev2_name'],
+                                          lev3_name=data['lev3_name'],
+                                          location=data['location']), data) for
+               data in dataset_list]
+    r = mycache.pipeline_hmset(dataset)
+    return r
